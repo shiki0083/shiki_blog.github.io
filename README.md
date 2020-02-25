c65fab3800e6d66741655ad1b22a9a1bdd599535
liujinlei <shiki0083@126.com>
2020-02-25 10:34:02 +0800

    Mod 1

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
+# Vue 构建的PC版本博客
+[![Build Status](https://travis-ci.org/hellomrbigshot/blog-frontend.svg?branch=master)](https://travis-ci.org/hellomrbigshot/blog-frontend)
+
+
+> A Vue.js project
+
 
 ## Build Setup
 
 ``` bash
 # install dependencies
-npm install
+yarn
 
 # serve with hot reload at localhost:8080
-npm run serve
+yarn serve
 
 # build for production with minification
-npm run build
+yarn build
 
 # build for production and view the bundle analyzer report
-npm run build --report
+yarn report
 ```
+## 介绍
+此项目是用```vue-cli```构造的[本人博客](http://vue.hellomrbigbigshot.xyz)的前端项目。
+
+## 相关链接
+### 后端项目
+[后端项目](https://github.com/hellomrbigshot/blog-pwa/tree/master/server)
+
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,43 @@
+const path = require('path');
+function resolve(dir) {
+	return path.join(__dirname, dir);
+}
 module.exports = {
 	runtimeCompiler: true,
+	productionSourceMap: false,
+	pages: {
+		index: {
+			entry: 'src/index/main.js',
+			template: 'public/index.html',
+			filename: 'index.html',
+			title: '世说新语',
+			chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+		}
+	},
 	devServer: {
+		port: 10086,
+		historyApiFallback: {
+			rewrites: [ { from: /^\/$/, to: '/index.html' } ],
+			disableDotRule: true
+		},
 		proxy: {
 			// '/api': {
-			//   target: '',
+			//   target: 'https://hellomrbigbigshot.xyz',
 			//   changeOrigin: true
-			// }
-			// '/api': {
-			//   target: 'http://localhost:3333',
-			//   changeOrigin: true
-			// }
+			// },
 			'^/api': {
-				target: 'http://localhost:3333',
+				target: 'http://localhost:8081',
 				ws: true,
 				changeOrigin: true
 			}
 		}
+	},
+	chainWebpack: (config) => {
+		config.module.rule('vue').use('vue-loader').loader('vue-loader').tap((options) => {
+			options.compilerOptions.preserveWhitespace = true;
+		});
+		config.resolve.alias
+			.set('@$', resolve('src'))
+			.set('index', resolve('src/index'))
 	}
 };
